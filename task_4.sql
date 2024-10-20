f3b32e951789a102c7c3cf063bba559135d034c7
Zaid-dev2000 <elouakhchachizaid@gmail.com>
2024-10-20 17:10:03 +0100

    Added task_4.sql to show the full description of the books table

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,16 @@
 -- Select the alx_book_store database
 USE alx_book_store;
 
--- Show the full description of the books table
-SHOW CREATE TABLE books;
+-- Query to print the full description of the 'books' table
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'books';
